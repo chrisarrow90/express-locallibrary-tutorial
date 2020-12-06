5af663bea7c5611c82871b6f5fdb3d8fb4eee038
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2020-12-06 15:30:17 +1300

    create BookSchema and export model 'Book'

---
+++ models/book.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema (
+  {
+    title: {type: String, required: true},
+    // ref option tells mongoose which model to use during population - using the ObjectId of 'Author'
+    author:{type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    // Genre will be an array of ObjectId's since may contain more than one genre
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
+  }
+)
+
+// Virtual for book URL
+BookSchema
+  .virtual(function () {
+    return '/catolog/book/' + this._id;
+  });
+
+// Export Model
+module.exports = mongoose.model('Book', BookSchema);
