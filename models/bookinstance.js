5e975c070c17f3e4d906f1541252e81e130848e9
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2020-12-07 22:45:25 +1300

    <build>: add module luxon to format date better for book due back date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -17,6 +18,10 @@ const BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual('url').get(function () {
   return `/catalog/bookinstance/${this._id}`; // eslint-disable-line no-underscore-dangle
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function dueBackFormatted() {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
