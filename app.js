dace1c2094004d73ff6380475805ecb4fdb8205f
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2020-12-10 19:02:55 +1300

    production version submitted. removed dev key (revoked).

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ const app = express();
 // Setup Mongoose Connetion
 const mongoose = require('mongoose');
 
-const dev_db_url =
-  'mongodb+srv://chrisarrow:8TjxNLR0S2t0Zz7I@cluster0.mw7jj.mongodb.net/local_library?retryWrites=true&w=majority';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
